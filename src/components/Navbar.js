ac6c8a7c60ce1168c08ed641b288fd74df24a893
kingsley ijomah <kingsley.ijomah@gmail.com>
2021-07-27 13:45:47 +0100

    Navbar-part1

---
+++ src/components/Navbar.js
@@ -0,0 +1,68 @@
+import { AppBar, Box, Tab, Tabs, Toolbar, Typography, IconButton, Badge } from '@material-ui/core'
+import React from 'react'
+
+import SearchIcon from '@material-ui/icons/Search';
+import NotificationsNoneIcon from '@material-ui/icons/NotificationsNone';
+import StarBorderIcon from '@material-ui/icons/StarBorder';
+import PersonOutlineIcon from '@material-ui/icons/PersonOutline';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+
+import { Link } from "react-router-dom"
+
+export default function Navbar() {
+  return (
+    <Box>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography component="h6">JOBPLUS</Typography>
+          <Tabs>
+            <Tab
+              key={0}
+              label={'Home'}
+              component={Link}
+              to={'/'}
+            />
+            <Tab
+              key={1}
+              label={'Job Listings'}
+              component={Link}
+              to={'/job-listings'}
+            />
+            <Tab
+              key={2}
+              label={'Job Applications'}
+              component={Link}
+              to={'/job-applications'}
+            />
+          </Tabs>
+
+          <Box>
+            <IconButton size="small" component={Link} to={'/search'} color="inherit" edge='start'>
+              <SearchIcon />
+            </IconButton>
+
+            <IconButton size="small" component={Link} to={'/notifications'} color="inherit" edge='start'>
+              <Badge color="error" overlap="circle" variant="dot">
+                <NotificationsNoneIcon />
+              </Badge>
+            </IconButton>
+
+            <IconButton size="small" component={Link} to={'/saved-jobs'} color="inherit" edge='start'>
+              <Badge badgeContent={2}>
+                <StarBorderIcon />
+              </Badge>
+            </IconButton>
+
+            <IconButton size="small" component={Link} to={'/search'} color="inherit" edge='start'>
+              <PersonOutlineIcon />
+            </IconButton>
+
+            <IconButton size="small" component={Link} to={'/search'} color="inherit" edge='start'>
+              <ExitToAppIcon />
+            </IconButton>
+          </Box>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  )
+}

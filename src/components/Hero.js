548ebb1d60711fa10b3ac4ca3abaab8a139b9e46
kingsley ijomah <kingsley.ijomah@gmail.com>
2021-07-31 12:51:10 +0100

    adding controls

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import Controls from './controls/Controls'
 
 export default function Hero() {
   return (
     <>
-      Hero component here...
+      <Controls.FormGroupCustom>
+        <Controls.ButtonCustom text='Search' />
+      </Controls.FormGroupCustom>
     </>
   )
 }

ba9db91af99925ef5855da8bb4b61829f214099c
kingsley ijomah <kingsley.ijomah@gmail.com>
2021-08-05 12:51:55 +0100

    Adding listings

--- src/components/Listing.js
+++ src/components/Listing.js
@@ -3,6 +3,10 @@ import { makeStyles } from '@material-ui/styles';
 import { Typography } from '@material-ui/core';
 import StarIcon from '@material-ui/icons/Star';
 import StarBorderIcon from '@material-ui/icons/StarBorder';
+import { Link } from "react-router-dom";
+import AttachMoneyIcon from '@material-ui/icons/AttachMoney';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+import TimerIcon from '@material-ui/icons/Timer';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -17,7 +21,6 @@ const useStyles = makeStyles((theme) => ({
     display: 'grid',
     gridTemplateRows: 'repeat(2, auto)',
     gridTemplateColumns: 'repeat(2, auto)',
-    gridGap: '0.5rem',
   },
   title: {
     gridRow: '1 / 2',
@@ -42,6 +45,39 @@ const useStyles = makeStyles((theme) => ({
       color: theme.palette.common.beta,
     }
   },
+  items: {
+    display: 'grid',
+    gridTemplateRows: 'repeat(2, 1fr)',
+    gridTemplateColumns: 'repeat(2, 1fr)',
+    gridGap: '0.5rem',
+    listStyle: 'none',
+    margin: '1.3rem 0',
+    padding: 0,
+    '& li': {
+      display: 'flex',
+      '& svg': {
+        fontSize: '20px',
+        marginRight: '0.5rem',
+      }
+    },
+    '& li:nth-of-type(2)': {
+      justifySelf: 'right'
+    }
+  },
+  details: {
+    fontSize: '0.9rem',
+    marginBottom: 0,
+    '& a': {
+      color: 'inherit',
+      textDecoration: 'none'
+    }
+  },
+  cta: {
+    fontSize: '0.9rem',
+    textAlign: 'right',
+    display: 'block',
+    color: 'inherit',
+  }
 }));
 
 export default function Listing() {
@@ -51,9 +87,22 @@ export default function Listing() {
     <div className={classes.root}>
       <header className={classes.header}>
         <Typography className={classes.title} variant='h1'>Regulatory Affairs Senior Manager</Typography>
-        <StarIcon className={classes.saved} />
+        <StarBorderIcon className={classes.saved} />
         <p className={classes.company}>Posted by <span>Koco Media</span></p>
       </header>
+
+      <ul className={classes.items}>
+        <li><AttachMoneyIcon /><b>Salary negotiable</b></li>
+        <li><LocationOnIcon />Heyes, <b>Uxbridge</b></li>
+        <li><TimerIcon />Contract, full-time</li>
+      </ul>
+
+      <p className={classes.details}>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+          Explicabo, asperiores ullam minus at <b><Link to="/">Read more...</Link></b>
+      </p>
+
+      <Link className={classes.cta} to="/">Withdraw application</Link>
     </div>
   )
 }
